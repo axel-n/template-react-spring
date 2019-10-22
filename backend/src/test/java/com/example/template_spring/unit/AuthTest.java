f4e58fc3efdf8bef056941af808c7a483c6973f0
Alex Anoshkin <a.anoshkin@auchan.ru>
2019-10-22 23:21:25 +0300

    переместил в подходящую папку

--- backend/src/test/java/com/example/template_spring/unit/AuthTest.java
+++ backend/src/test/java/com/example/template_spring/unit/AuthTest.java
@@ -1,4 +1,4 @@
-package com.example.template_spring;
+package com.example.template_spring.unit;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
@@ -17,6 +18,7 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@PropertySource("classpath:application.properties ")
 public class AuthTest {
 
     @Autowired
@@ -30,7 +32,7 @@ public class AuthTest {
     @Test
     public void validPass() throws JsonProcessingException {
         Map<String, Object> params = new HashMap<>();
-        params.put("username", "user1@gmail.com");
+        params.put("username", "user@gmail.com");
         params.put("password", 123);
 
         EntityExchangeResult<Map> response = client.post()
